a24e1b2ac64b9a8cf73b0ae001752237df13b2de
Abhimanyu Kumar <abhimanyukrsingh00@gmail.com>
2019-04-27 13:15:08 +0530

    arranged membership form properly

--- adminp/home.php
+++ adminp/home.php
@@ -33,6 +33,7 @@ else
   <link href="css/simple-sidebar.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/fixedcolumns/3.2.6/css/fixedColumns.dataTables.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 
 <body>
@@ -57,33 +58,14 @@ else
     <div id="page-content-wrapper">
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+        <button id="menu-toggle" class="navbar-toggler" style="display: inline-block !important;outline: none;"><i class="fas fa-bars"></i></button>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
             <li class="nav-item active">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
-                Dropdown
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
           </ul>
-        </div>
       </nav>
 
       <div class="container-fluid load">
--- adminp/member.php
+++ adminp/member.php
@@ -1,102 +1,140 @@
-<h2>Add Member</h2>
-
-<form action="./add_member.php" method="POST" accept-charset="utf-8" enctype="multipart/form-data">
-	<label>Name:</label>
-	<input type="text" name="name"><br><br>
-	Upload Photo:
-	<input type="file" name="photo"><br>
-	Date Of Birth:
-	<input type="date" name="dob"><br>
-	Age:
-	<input type="text" name="age"><br>
-	Address:
-	<textarea name="address"></textarea><br>
-	Contact:
-	<input type="text" name="contact"><br>
-	Pan Id No:
-	<input type="text" name="panid"><br>
-	Email:
-	<input type="email" name="email"><br>
-	Occupation/Qualification(Mention Stream & Semester):
-	<textarea name="occ"></textarea><br>
-	If currently in school, include name of school, grade and course of study:
-	<textarea name="school"></textarea><br>
-	Are you a current member of any other Animal Welfare Society/Club in India or Abroad? If so please mention the club and status:
-	<input type="text" name="currclub"><br>
-	SOCIETY:
-	<input type="text" name="society"><br>
-	Status:
-	<input type="text" name="status"><br>
-	Are you existing member of PFA Durg-Bhilai Unit II: (YES/NO) If Yes, then share your Membership Number:
-	<input type="text" name="memnum"><br>
-	<table>
-		<thead>
-			<tr>
-				<th>Membership Type</th>
-				<th>Membership Fee</th>
-				<th>Benefits</th>
-				<th>Duration</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>Half Yearly</td>
-				<td>600</td>
-				<td>ID Cards for 6 months</td>
-				<td>6M</td>
-				<td><input type="radio" name="mem_type" value="6m"></td>
-			</tr>
-			<tr>
-				<td>Yearly</td>
-				<td>1200</td>
-				<td>ID Cards for 1 Year</td>
-				<td>12M</td>
-				<td><input type="radio"  name="mem_type" value="12m"></td>
-			</tr>
-		</tbody>
-	</table><br>
-	<label>Area Of Interest:</label>
-	<input type="checkbox" value="Field" name="aoi[]" value="">
-	<label>1. Field</label>
-	<input type="checkbox" value="EVent Participation" name="aoi[]" value="">
-	<label>2. Event Participation</label>
-	<input type="checkbox" value="HR" name="aoi[]" value="">
-	<label>3. HR</label>
-	<input type="checkbox" value="Donation" name="aoi[]" value="">
-	<label>4. Donation</label>
-	Rules:
-	1) Field- Must attend (come for help) at least 2 cases in a month or help in Foster home management/cleaning at least twice a month.
-   Note- This does not require any trained first aid specialiation. Any 3-4 people can come together and take the animal to our recommended vet. Expenses will be paid by PFA.
-2) Event – Must conduct or attend at least 1 event in a month. E.g. Awareness campaign, Adoption camp, sterilization drive, Plantation drives etc.
-3)HR- Attend calls, Attend Public meetings, Follow up on adopters, Sponsors, Donors, Members etc, Public speaking and giving presentations etc.
-4) Donation- If not able to contribute any of the above activities, please donate any small amount every month apart from membership fees.
-
-
-
-<label>I understand that People for Animals Durg-Bhilai unit-II reserves the right to terminate my membership status as a result of any of the following	
-</label>
-<div class="tandc">
-	⦁	Any abuse or mistreatment of any animal
-⦁	Failure to comply with organization policies, rules and other regulations
-⦁	Unsatisfactory attitude, work or appearance
-⦁	Any other circumstances which, in judgement of President/ Secretary would make my continued service as a member
-⦁	Hold absolutely confidential information that I may obtain concerning clients animals and staff. I understand that an intentional or unintentional violation of confidentiality may result in 
-disciplinary action, including termination by PFA Durg-Bhilai unit-II and/or legal action by other
-
-
-<input type="checkbox" name="agree" value="1">
-
-I have read and understood each of above conditions. My signature below indicates that I agree to comply with them.
-
+<div class="page-header">
+<h1>Add Member</h1>      
 </div>
+<form action="./add_member.php" method="POST" accept-charset="utf-8" enctype="multipart/form-data">
 
-<input type="submit" value="Submit" required>
+	<div class="form-group">
+  		<label for="name">Name:</label>
+	  	<input type="text" class="form-control" name="name">
+	</div>
+	<div class="custom-file">
+	    <input type="file" class="custom-file-input" name="photo" aria-describedby="inputGroupFileAddon01">
+	    <label class="custom-file-label" for="inputGroupFile01">Upload Photo</label>
+    </div>
+	<div class="form-group">
+  		<label for="dob"><br>Date Of Birth:</label>
+	  	<input type="date" class="form-control" name="dob">
+	</div>
+	<div class="form-group">
+  		<label for="age">Age</label>
+	  	<input type="number" class="form-control" name="age">
+	</div>
+	<div class="form-group">
+  		<label for="address">Address:</label>
+	  	<textarea class="form-control" name="address"></textarea>
+	</div>
+	<div class="form-group">
+  		<label for="contact">Contact:</label>
+	  	<input type="text" class="form-control" name="contact">
+	</div>
+	<div class="form-group">
+  		<label for="panid">Pan Id No:</label>
+	  	<input type="text" class="form-control" name="panid">
+	</div>
+	<div class="form-group">
+  		<label for="email">Email:</label>
+	  	<input  type="email" name="email" class="form-control" >
+	</div>
+	<div class="form-group">
+  		<label for="occ">Occupation/Qualification(Mention Stream & Semester):</label>
+	  	<textarea class="form-control" name="occ"></textarea>
+	</div>
+	<div class="form-group">
+  		<label for="school">If currently in school, include name of school, grade and course of study:</label>
+	  	<textarea class="form-control" name="school"></textarea>
+	</div>
+	<div class="form-group">
+  		<label for="currclub">Are you a current member of any other Animal Welfare Society/Club in India or Abroad? If so please mention the club and status:</label>
+	  	<input type="text" class="form-control" name="currclub">
+	</div>
+	<div class="form-group">
+  		<label for="society">SOCIETY:</label>
+	  	<input type="text" class="form-control" name="society">
+	</div>
+	<div class="form-group">
+  		<label for="status">Status:</label>
+	  	<input type="text" class="form-control" name="status">
+	</div>
+	<div class="form-group">
+  		<label for="memnum">Are you existing member of PFA Durg-Bhilai Unit II: (YES/NO) If Yes, then share your Membership Number::</label>
+	  	<input type="text" class="form-control" name="memnum">
+	</div>
+
+	<div class="form-group">
+		<label for="mem_type">Choose Memebership Type:</label>
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>Select</th>
+					<th>Membership Type</th>
+					<th>Membership Fee</th>
+					<th>Benefits</th>
+					<th>Duration</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr onclick="document.getElementById('member1').checked = true;">
+					<td><input type="radio" id="member1" name="mem_type" value="6m"></td>
+					<td>Half Yearly</td>
+					<td>600</td>
+					<td>ID Cards for 6 months</td>
+					<td>6M</td>					
+				</tr>
+				<tr onclick="document.getElementById('member2').checked = true;">
+					<td><input type="radio" id="member2" name="mem_type" value="12m"></td>
+					<td>Yearly</td>
+					<td>1200</td>
+					<td>ID Cards for 1 Year</td>
+					<td>12M</td>					
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<br>
+
+	<div class="form-group">
+  		<label for="aoi">Choose area Of Interest:</label>
+	  	<label class="checkbox-inline"><input type="checkbox" value="Field" name="aoi[]">&nbspField</label>
+		<label class="checkbox-inline"><input type="checkbox" value="EVent Participation"  name="aoi[]">&nbspEvent Participation</label>
+		<label class="checkbox-inline"><input type="checkbox" value="HR" name="aoi[]">&nbspHR</label> 
+		<label class="checkbox-inline"><input type="checkbox" value="Donation" name="aoi[]">&nbspDonation</label> 
+	</div>
+
+
+  <ol class="list-group">
+  	<li class="list-group-item list-group-item-info">Rules</li>
+    <li class="list-group-item">Field- Must attend (come for help) at least 2 cases in a month or help in Foster home management/cleaning at least twice a month.<br>
+   	<b>Note:</b> This does not require any trained first aid specialiation. Any 3-4 people can come together and take the animal to our recommended vet. Expenses will be paid by PFA.</li>
+    <li class="list-group-item">Event – Must conduct or attend at least 1 event in a month. E.g. Awareness campaign, Adoption camp, sterilization drive, Plantation drives etc.</li>
+    <li class="list-group-item">HR- Attend calls, Attend Public meetings, Follow up on adopters, Sponsors, Donors, Members etc, Public speaking and giving presentations etc.</li>
+    <li class="list-group-item">Donation- If not able to contribute any of the above activities, please donate any small amount every month apart from membership fees.</li>
+  </ol>
+
+<br><br>
+	<div class="tandc">
+	  <ul class="list-group">
+	  	<li class="list-group-item list-group-item-success">I understand that People for Animals Durg-Bhilai unit-II reserves the right to terminate my membership status as a result of any of the following:</li>
+	    <li class="list-group-item">Any abuse or mistreatment of any animal</li>
+	    <li class="list-group-item">Failure to comply with organization policies, rules and other regulations</li>
+	    <li class="list-group-item">Unsatisfactory attitude, work or appearance</li>
+	    <li class="list-group-item">Any other circumstances which, in judgement of President/ Secretary would make my continued service as a member</li>
+	    <li class="list-group-item">Hold absolutely confidential information that I may obtain concerning clients animals and staff. I understand that an intentional or unintentional violation of confidentiality may result in disciplinary action, including termination by PFA Durg-Bhilai unit-II and/or legal action by other</li>
+	  </ul>	
+	</div>
+	<br>
+	<div class="form-group">
+	  	<label class="checkbox-inline"><input type="checkbox" value="1" name="agree">&nbspI have read and understood each of above conditions. My signature below indicates that I agree to comply with them.</label>
+	</div>
+
+
+	<div class="form-group">
+		<input type="submit" class="btn btn-success" value="Submit" required>
+	</div>
 
 </form>
+
 <?php 
-
-
 session_start();
 //echo $_SESSION['curr_page_class'];
 $_SESSION['curr_page_class'] = '.add_member';
