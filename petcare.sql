d254be052bd553a664091529ea525aad7a671de2
jishant07 <jishanta@gmail.com>
2019-05-02 11:45:43 +0530

    Rescue Form added & CaseID generated

--- petcare.sql
+++ petcare.sql
@@ -1,24 +1,28 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2019 at 09:35 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: May 02, 2019 at 08:14 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `petcare`
 --
+CREATE DATABASE IF NOT EXISTS `petcare` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `petcare`;
 
 -- --------------------------------------------------------
 
@@ -26,12 +30,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin_details`
 --
 
-CREATE TABLE IF NOT EXISTS `admin_details` (
-`SR` int(11) NOT NULL,
+CREATE TABLE `admin_details` (
+  `SR` int(11) NOT NULL,
   `NAME` text NOT NULL,
   `USERNAME` text NOT NULL,
   `PASSWORD` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin_details`
@@ -42,7 +46,8 @@ INSERT INTO `admin_details` (`SR`, `NAME`, `USERNAME`, `PASSWORD`) VALUES
 (2, 'abhimanyu', 'abhi', '123'),
 (3, 'jishant', 'mangs', '123'),
 (4, 'abc', 'abc', '202cb962ac59075b964b07152d234b70'),
-(5, 'xyz', 'xyz', '202cb962ac59075b964b07152d234b70');
+(5, 'xyz', 'xyz', '202cb962ac59075b964b07152d234b70'),
+(6, '', '', 'd41d8cd98f00b204e9800998ecf8427e');
 
 -- --------------------------------------------------------
 
@@ -50,14 +55,14 @@ INSERT INTO `admin_details` (`SR`, `NAME`, `USERNAME`, `PASSWORD`) VALUES
 -- Table structure for table `adopter_details`
 --
 
-CREATE TABLE IF NOT EXISTS `adopter_details` (
-`id` int(11) NOT NULL,
+CREATE TABLE `adopter_details` (
+  `id` int(11) NOT NULL,
   `name` text NOT NULL,
   `contact` text NOT NULL,
   `email` text NOT NULL,
   `address` text NOT NULL,
   `anim_sr` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `adopter_details`
@@ -73,15 +78,15 @@ INSERT INTO `adopter_details` (`id`, `name`, `contact`, `email`, `address`, `ani
 -- Table structure for table `adoption`
 --
 
-CREATE TABLE IF NOT EXISTS `adoption` (
-`sr` int(11) NOT NULL,
+CREATE TABLE `adoption` (
+  `sr` int(11) NOT NULL,
   `animal` text NOT NULL,
   `age` int(11) NOT NULL,
   `gender` text NOT NULL,
   `breed` text NOT NULL,
   `photo_location` text NOT NULL,
   `adopt_status` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `adoption`
@@ -99,8 +104,8 @@ INSERT INTO `adoption` (`sr`, `animal`, `age`, `gender`, `breed`, `photo_locatio
 -- Table structure for table `lost_found`
 --
 
-CREATE TABLE IF NOT EXISTS `lost_found` (
-`id` int(11) NOT NULL,
+CREATE TABLE `lost_found` (
+  `id` int(11) NOT NULL,
   `animal` text NOT NULL,
   `animal_details` text NOT NULL,
   `owner_name` text NOT NULL,
@@ -109,16 +114,18 @@ CREATE TABLE IF NOT EXISTS `lost_found` (
   `photo_location` text NOT NULL,
   `status` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lost_found`
 --
 
 INSERT INTO `lost_found` (`id`, `animal`, `animal_details`, `owner_name`, `contact`, `address`, `photo_location`, `status`, `timestamp`) VALUES
-(1, 'dog', '', 'ohm trivedi', '7639195879', 'lksndfnsf', '/uploads/lost_found/anim1.jpeg', 0, '2019-04-28 07:45:56'),
+(1, 'dog', '', 'ohm trivedi', '7639195879', 'lksndfnsf', '/uploads/lost_found/anim1.jpeg', 1, '2019-04-28 07:45:56'),
 (2, 'dog', '', 'ohm trivedi', '7639195879', 'lksndfnsf', '/uploads/lost_found/anim2.jpeg', 0, '2019-04-28 07:46:14'),
-(3, 'cat', 'lklnsfl lnasdfd ls vlksandfl l asldnffls flas dflnsaldflasd dla sdlf las dfl', 'ohm trivedi', '7639195879', 'lksndfnsf', '/uploads/lost_found/anim3.jpeg', 0, '2019-04-28 08:03:24');
+(3, 'cat', 'lklnsfl lnasdfd ls vlksandfl l asldnffls flas dflnsaldflasd dla sdlf las dfl', 'ohm trivedi', '7639195879', 'lksndfnsf', '/uploads/lost_found/anim3.jpeg', 0, '2019-04-28 08:03:24'),
+(4, '', '', '', '', '', '/uploads/lost_found/anim4.', 1, '2019-05-01 13:56:18'),
+(5, '', '', '', '', '', '/uploads/lost_found/anim5.', 1, '2019-05-01 13:59:43');
 
 -- --------------------------------------------------------
 
@@ -126,8 +133,8 @@ INSERT INTO `lost_found` (`id`, `animal`, `animal_details`, `owner_name`, `conta
 -- Table structure for table `member`
 --
 
-CREATE TABLE IF NOT EXISTS `member` (
-`sr` int(11) NOT NULL,
+CREATE TABLE `member` (
+  `sr` int(11) NOT NULL,
   `name` text NOT NULL,
   `dob` text NOT NULL,
   `age` int(11) NOT NULL,
@@ -147,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `member` (
   `photo_location` text NOT NULL,
   `reg_timestamp` text NOT NULL,
   `payment` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `member`
@@ -157,6 +164,22 @@ INSERT INTO `member` (`sr`, `name`, `dob`, `age`, `address`, `contact`, `panid`,
 (3, 'ohm trivedi', '2019-04-23', 25, 'lksndfnsf', '7639195879', '22563321235165', 'ohm.trivedi@yahoo.com', ' kn sakdf k sdkfs', '', '', '', '', '', '6m', 'EP,Donation', 1, './uploads/member/m3.jpeg', '1556274748', 1),
 (4, 'abhimanyu', '2019-04-09', 20, 'sdfscasfdsdc', '7639195879', '22563321235165', 'ohm.trivedi@yahoo.com', 'facas fdfascas dcsadc d', 'sdcaslm cksa dksa d ksaf', '', '', '', '', '12m', 'EP,HR', 1, './uploads/member/m4.jpeg', '1556274886', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rescue`
+--
+
+CREATE TABLE `rescue` (
+  `case_id` varchar(100) NOT NULL DEFAULT '',
+  `case_title` text NOT NULL,
+  `case_date` text NOT NULL,
+  `case_address` text NOT NULL,
+  `case_contact` text NOT NULL,
+  `contact_name` text NOT NULL,
+  `case_problem` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -165,31 +188,38 @@ INSERT INTO `member` (`sr`, `name`, `dob`, `age`, `address`, `contact`, `panid`,
 -- Indexes for table `admin_details`
 --
 ALTER TABLE `admin_details`
- ADD PRIMARY KEY (`SR`);
+  ADD PRIMARY KEY (`SR`);
 
 --
 -- Indexes for table `adopter_details`
 --
 ALTER TABLE `adopter_details`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `adoption`
 --
 ALTER TABLE `adoption`
- ADD PRIMARY KEY (`sr`);
+  ADD PRIMARY KEY (`sr`);
 
 --
 -- Indexes for table `lost_found`
 --
 ALTER TABLE `lost_found`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `member`
 --
 ALTER TABLE `member`
- ADD PRIMARY KEY (`sr`), ADD UNIQUE KEY `sr` (`sr`);
+  ADD PRIMARY KEY (`sr`),
+  ADD UNIQUE KEY `sr` (`sr`);
+
+--
+-- Indexes for table `rescue`
+--
+ALTER TABLE `rescue`
+  ADD PRIMARY KEY (`case_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -199,27 +229,28 @@ ALTER TABLE `member`
 -- AUTO_INCREMENT for table `admin_details`
 --
 ALTER TABLE `admin_details`
-MODIFY `SR` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `SR` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `adopter_details`
 --
 ALTER TABLE `adopter_details`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `adoption`
 --
 ALTER TABLE `adoption`
-MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `lost_found`
 --
 ALTER TABLE `lost_found`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- rescue_req.php
+++ rescue_req.php
@@ -4,19 +4,19 @@ $sql = "select * from rescue";
 $result = $con->query($sql);
 //Case Number AutoGeneration
 $num_of_rows = mysqli_num_rows($result);
-if ($num_of_rows<10) 
+if(($num_of_rows+1)<10)
 {
 	$case_id = "PET000".($num_of_rows+1);
 }
-else if($num_of_rows<100)
+if((($num_of_rows+1)<100) && (($num_of_rows+1)>=10))
 {
 	$case_id = "PET00".($num_of_rows+1);
 }
-else if($num_of_rows<1000)
+if((($num_of_rows+1)<1000) && (($num_of_rows+1)>=100))
 {
 	$case_id = "PET0".($num_of_rows+1);	
 }
-else
+if(($num_of_rows+1)>=1000)
 {
 	$case_id = "PET".($num_of_rows+1);		
 }
@@ -29,8 +29,7 @@ $contact_num = $_POST["contact"];
 $address = $_POST["animal_address"];
 $problem = $_POST["animal_problem"];
 //Add Query 
-$sql_add_query = "insert into rescue values (`$case_id`,`$case_title`,`$case_date`,`$address`
-												,`$contact_num`,`$name`,`$problem`)";
-echo $sql_add_query;
-//$result = $con->query($sql_add_query);
+$sql_add_query = "insert into rescue values ('$case_id','$case_title','$case_date','$address'
+												,'$contact_num','$name','$problem')";
+$con->query($sql_add_query);
 ?>
